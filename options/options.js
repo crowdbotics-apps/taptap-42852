e10b9ee87bf0cf12635a5e4309c0c967c7eca724
Crowdbotics <team@crowdbotics.com>
2023-08-15 05:14:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "taptap-42852",
+  url: "https://taptap-42852.botics.co",
+  api: "https://taptap-42852.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
